53b781c60c1353df323c6ee2b19536dc58831dd0
Seah Bei Ying <beiyingseah@gmail.com>
2022-02-05 14:42:37 +0800

    Added radial gradients to calculator and web page background

---
+++ main.js
@@ -0,0 +1,28 @@
+// Add 
+function add(stored_value, input) {
+    let result = stored_value + input;
+    return result;
+}
+
+// Subtract
+function subtract(stored_value,input) {
+    let result = stored_value - input;
+    return result;
+}
+
+// Multiply
+function mutiply(stored_value, input) {
+    let result = stored_value * input;
+    return result;
+}
+
+// Divide
+function divide(stored_value, input) {
+    let result = stored_value / input;
+    return result;
+}
+
+
+/* Comments
+Is there a way to dynamically add the operators based on user's operator input?
+*/
