f32cad252c189688c41b0da77282441466ea8c58
Seah Bei Ying <beiyingseah@gmail.com>
2022-02-05 19:27:19 +0800

    Added basic math operator functions + max. limit of decimal display

--- main.js
+++ main.js
@@ -1,28 +1,53 @@
 // Add 
 function add(stored_value, input) {
-    let result = stored_value + input;
-    return result;
+    return stored_value += input;
 }
 
 // Subtract
 function subtract(stored_value,input) {
-    let result = stored_value - input;
-    return result;
+    return stored_value -= input
 }
 
 // Multiply
-function mutiply(stored_value, input) {
-    let result = stored_value * input;
-    return result;
+function multiply(stored_value, input) {
+    return stored_value *= input
 }
 
 // Divide
 function divide(stored_value, input) {
-    let result = stored_value / input;
-    return result;
+    return stored_value /= input;
+}
+
+function operate(operator, stored_value, input) {
+    stored_value = Number(stored_value);
+    input = Number(input);
+    switch (operator) {
+        case '/':
+            if (input === 0) return NaN;
+            else return divide(stored_value, input);
+        case '*':
+            return multiply(stored_value, input);
+        case '-':
+            return subtract(stored_value, input);
+        case '+':
+            return add(stored_value, input);
+        default:
+            return null;
+    }
 }
 
 
-/* Comments
-Is there a way to dynamically add the operators based on user's operator input?
-*/
+// Main 
+
+
+// List of interactive variables
+/* Events to capture */
+/* SCREEN READOUT INTERACTIVITY */
+/* COLOUR BUTTONS INTERACTIVITY */
+
+/* RECAP */
+// A string, representing the underlying value that is associated with the button. Type conversion to Number required!
+
+
+/* COMMENTS */
+//Is there a way to dynamically add the operators based on user's operator input?
