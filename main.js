8bf7a0d2f9844959fb5c4c1eb5f543ed1fceac44
Seah Bei Ying <beiyingseah@gmail.com>
2022-02-19 20:58:26 +0800

    Implemented readout interactivity for number buttons + max. limit of digits that can be displayed + decimal place recognition

--- main.js
+++ main.js
@@ -1,49 +1,88 @@
 // Add 
-function add(stored_value, input) {
-    return stored_value += input;
+function add(stored_value, num) {
+    return stored_value += num;
 }
 
 // Subtract
-function subtract(stored_value,input) {
-    return stored_value -= input
+function subtract(stored_value, num) {
+    return stored_value -= num
 }
 
 // Multiply
-function multiply(stored_value, input) {
-    return stored_value *= input
+function multiply(stored_value, num) {
+    return stored_value *= num
 }
 
 // Divide
-function divide(stored_value, input) {
-    return stored_value /= input;
+function divide(stored_value, num) {
+    return stored_value /= num;
 }
 
-function operate(operator, stored_value, input) {
+function operate(operator, stored_value, digit) {
     stored_value = Number(stored_value);
-    input = Number(input);
+    digit = Number(digit);
     switch (operator) {
         case '/':
             if (input === 0) return NaN;
-            else return divide(stored_value, input);
+            else return divide(stored_value, digit);
         case '*':
-            return multiply(stored_value, input);
+            return multiply(stored_value, digit);
         case '-':
-            return subtract(stored_value, input);
+            return subtract(stored_value, digit);
         case '+':
-            return add(stored_value, input);
+            return add(stored_value, digit);
         default:
             return null;
     }
 }
 
+/* List of interactive variables */ 
+const readout = document.getElementById('readout');
+const digits = document.querySelectorAll('.digit'); 
+const clearAllBtn = document.getElementById('clearAllBtn');
+const clearEntryBtn = document.getElementById('clearEntryBtn')
 
-// Main 
-
-
-// List of interactive variables
 /* Events to capture */
+// Digits
+digits.forEach((digit) => {
+    digit.addEventListener('click', function() {
+        if (readout.textContent.length === 16) {
+            return null;
+        }
+
+        // Limit Readout to one variable
+        if ((readout.textContent.indexOf(".") !== -1) && digit.value === '.') {
+            return null;
+        }
+         
+        else {
+            printReadout(digit.value);
+        }
+    })
+});
+
+// Clear buttons
+clearAllBtn.onclick = () => clearReadout();
+clearEntryBtn.onclick = () => backspaceReadout();
+
+
 /* SCREEN READOUT INTERACTIVITY */
+function printReadout(digit) {
+    readout.textContent += digit;
+}
+
+function clearReadout() {
+    readout.textContent = '';
+}
+
+function backspaceReadout() {
+    readout.textContent = readout.textContent.substring(0,readout.textContent.length - 1);
+}
+
 /* COLOUR BUTTONS INTERACTIVITY */
+
+/* MAIN */
+
 
 /* RECAP */
 // A string, representing the underlying value that is associated with the button. Type conversion to Number required!
